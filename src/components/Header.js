58bffde86d573d0aa9e0e38bf04dbee2b9c295d9
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-02 18:12:29 +0300

    give filter buttons functionality where the page rerender a filtered output

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header({selected, removeSelected, clearAll}){
     console.log(selected)
     const myLanguages =selected.map((language)=>{
         return<ul>
-             <li>{language}
+             <li key={language}>{language}
         <button onClick={()=>removeSelected(language)}>
             <img src="./images/3b5b217ce258702c0956cbf3b146acba_t.jpeg" alt="delete-icon"/>
         </button>
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -3,13 +3,40 @@ import JobItem from "./JobItem";
 
 function JobList({jobs, selectLanguage, languages}){
 
-    const renderJobs = jobs.map((job)=>{
+    let [filteredData, setFilteredData] = useState([])
+       useEffect(()=>{
+        filteredJobs()
+
+    },[languages])
+
+
+
+    function filteredJobs(){
+        if(languages.length>0){
+
+            const newData = jobs.filter((job)=>{
+                return languages.every((lang)=>{
+                    return job.languages.includes(lang)
+                })
+            })
+            
+                setFilteredData(newData)
+
+                }else{
+            setFilteredData(jobs)
+           }
+   
+    }
+
+ 
+
+     const jobItems = filteredData.map((job)=>{
         return <JobItem key={job.id} data={job} selectLanguage={selectLanguage}/>
     })
     
     return(
         <div className="JobList-Container">
-            {renderJobs}
+            {jobItems}
         </div>
     )
 }

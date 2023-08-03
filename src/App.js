90b3fd1369813d3895229a87f6d0d4ccfc771afd
jeremyimbisi <jeremyimbisi44@gmail.com>
2023-08-03 14:38:27 +0300

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-// import { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-// import './App.css';
-// import EventCreate from './components/events/EventCreate';
-// import EventDetail from './components/events/EventDetail';
-// import EventEdit from './components/events/EventEdit';
-// import EventList from './components/events/EventList';
-// import { Nav } from './components/navbar/Nav';
-// import { Hero } from './components/pages/Hero';
-// import Footer from './components/footer/Footer';
-// import Login from './components/auth/Login';
-// import Signup from './components/auth/Signup';
-// import Logout from './components/auth/Logout';
-// import EditReview from './components/reviews/EditReview';
-// import CreateReview from './components/reviews/CreateReview';
-// import ListReview from './components/reviews/ListReview';
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Nav } from './Components/Navbar';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
-import Navbar from './Components/Navbar';
 import Signup from './Components/Signup';
 
 function App() {
@@ -43,23 +29,15 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Nav isAuthenticated={isAuthenticated} />
-        {/* <Hero /> */}
+        <Navbar isAuthenticated={isAuthenticated} /> {/* Render Navbar outside Routes */}
+        {/* Other components and routes */}
         <Routes>
-          {/* <Route path="/" element={<EventList />} />
-          <Route path="/events/create" element={<ProtectedRoute element={EventCreate} />} />
-          <Route path="/events/edit/:id" element={<ProtectedRoute element={EventEdit} />} />
-          <Route path="/events/detail/:id" element={<ProtectedRoute element={EventDetail} />} />
-          <Route path="/reviews/create" element={<ProtectedRoute element={CreateReview} />} />
-          <Route path="/reviews/edit/:id" element={<ProtectedRoute element={EditReview} />} />
-          <Route path="/reviews" element={<ProtectedRoute element={ListReview} />} /> */}
+          {/* Your other routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/logout" element={<Logout />} />
-          <Route path="/Navbar" element={<Navbar />} />
         </Routes>
       </div>
-      {/* <Footer /> */}
     </Router>
   );
 }
